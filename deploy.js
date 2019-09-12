2090f880167f2fed12f47125dc5d5bdd193da2b5
Naser Mirzaei <nasermirzaei89@gmail.com>
2019-09-12 12:43:54 +0430

    update packages

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const Lottery = require('./compile');
 
